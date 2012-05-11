8d5655a46f27304662d5831724a2eb70b16c2ab8
J RG <jrg@statsbiblioteket.dk>
2012-05-11 13:33:39 +0200

    temporary version to test with Taverna

--- yousee-downloader.sh
+++ yousee-downloader.sh
@@ -2,8 +2,8 @@
 
 ### Example input-parameters
 # CONFIGFILE='./example-configfile.sh'
-YOUSEENAME='DR1_20111123_212500_20111123_214500.mux'
-LOCALNAME='dr1_20111123212500_20111123214500.mux'
+#YOUSEENAME='TVT_20111123_212500_20111123_214500.mux'
+#LOCALNAME='dr1_20111123212500_20111123214500.mux'
 #
 ### Output goes to stdout
 
@@ -22,21 +22,24 @@ YOUSEE_URL_TO_FILE="http://canopus/yousee/${YOUSEENAME}"
 # Any errors of md5sum go to &4 (and fizzle)
 # Any remaining errors go to $error
 exec 3>&1 4>&2  # Set up extra file descriptors
-error=$( { curl -f -s -S $YOUSEE_URL_TO_FILE | tee "${LOCALPATH}/${LOCALNAME}" | md5sum 2>&4 1>"${LOCALNAME}.md5"; } 2>&1 )
+ERRORS=$( { curl -f -s -S $YOUSEE_URL_TO_FILE | tee "${LOCALPATH}/${LOCALNAME}" | md5sum 2>&4 1>"${LOCALPATH}/${LOCALNAME}.md5"; } 2>&1 4>&1 )
 exec 3>&- 4>&-  # Release the extra file descriptors
 
-FILESIZE=$(stat -c%s "${LOCALPATH}/${LOCALNAME}")
+ERRORS_LENGTH=${#ERRORS}
 
-echo '{'
-echo "   \"localFileUrl\" : \"file://${LOCALPATH}/${LOCALNAME}\","
-echo "   \"checksum\" : \"`cat ${LOCALPATH}/${LOCALNAME}.md5 | sed 's| .*||'`\","
-echo "   \"fileSize\" : $FILESIZE"
-echo '}'
+if [ "$ERRORS_LENGTH" -gt "0" ]; then
+	echo 'YouSee-downloader failed:'
+	echo "\"${ERRORS}\""
+	exit 13
+else
+	FILESIZE=$(stat -c%s "${LOCALPATH}/${LOCALNAME}")
+	echo '{'
+	echo "   \"localFileUrl\" : \"file://${LOCALPATH}/${LOCALNAME}\","
+	echo "   \"checksum\" : \"`cat ${LOCALPATH}/${LOCALNAME}.md5 | sed 's| .*||'`\","
+	echo "   \"fileSize\" : $FILESIZE"
+	echo '}'
+fi
 
 rm "${LOCALPATH}/${LOCALNAME}.md5"
-
-#echo "The error message is \"${error}\"."
-#echo "The checksum is \"${checksum}\"."
-
-
+exit 0
 
