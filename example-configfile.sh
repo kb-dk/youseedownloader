3a6e6c7a4fae8a29b1adda01c57d0c5157626e16
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-05-15 08:43:29 +0200

    removed the old files

--- example-configfile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# URL to the YouSee server-location where we can download mux-files
-URL_TO_YOUSEE='http://canopus/yousee'
-
-# Path of the place to download mux-files to
-LOCALPATH='/home/jeppe/projects/yousee-file-downloader/'
-
