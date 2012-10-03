abd85a6bf7a2fdcbf0e5d04fe30c4b205afa1b5c
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2012-10-03 17:48:15 +0200

    A hopefully correct downloader that wont redownload

--- src/main/script/yousee-downloader.sh
+++ src/main/script/yousee-downloader.sh
@@ -40,38 +40,76 @@ URL_TO_YOUSEE=${URL_TO_YOUSEE%/}  # remove trailing slash if there is one
 LOCALPATH=${LOCALPATH%/}          # remove trailing slash if there is one
 YOUSEE_URL_TO_FILE="${URL_TO_YOUSEE}/${YOUSEENAME}"
 
-# Relevant curl options:
-# -f  To make HTTP errors turn into a curl error 22, mentioning the HTTP error number
-# -s  Silent mode
-# -S  Show errors when in silent mode
+function isOK(){
+    OURCHECKSUM=`cat ${LOCALPATH}/${LOCALNAME}.md5 | cut -d' ' -f1` 2>/dev/null
+    THEIRCHECKSUM=`cat ${LOCALPATH}/${LOCALNAME}.headers  | grep -i "content-md5:" | cut -d' ' -f2 |  sed 's/\s*$//g' | base64 -d` 2>/dev/null
+    LOCALCHECKSUM=`md5sum ${LOCALPATH}/${LOCALNAME} | cut -d' ' -f1` 2>/dev/null
+    if [ $OURCHECKSUM -a $OURCHECKSUM == $THEIRCHECKSUM -a $OURCHECKSUM == $LOCALCHECKSUM -a $THEIRCHECKSUM == $LOCALCHECKSUM ];
+    then
+        return 0
+    else
+        return 1
+    fi
+}
 
-# Any errors of md5sum go temporarily to &3 in order to not overwrite errors from curl
-# All errors are collected in ERRORS
-exec 3>&1  # Set up extra file descriptor
-ERRORS=$( { curl -f -s -S "$YOUSEE_URL_TO_FILE" | tee "${LOCALPATH}/${LOCALNAME}" | md5sum 2>&3 1>"${LOCALPATH}/${LOCALNAME}.md5"; } 2>&1 3>&1 )
-exec 3>&-  # Release the extra file descriptor
+DOWNLOAD="yes"
+if [ -e ${LOCALPATH}/${LOCALNAME} ]; then
+    if ! isOK ; then
+        echo "File was found locally, but checksums do not match. Redownload" >&2
+        echo "checksum on stream: $OURCHECKSUM" >&2
+        echo "checksum from yousee: $THEIRCHECKSUM" >&2
+        echo "checksum calculated on disk: $LOCALCHECKSUM" >&2
+    else
+        DOWNLOAD=""
+    fi
+fi
 
-# The above was inspired by http://stackoverflow.com/questions/962255/how-to-store-standard-error-in-a-variable-in-a-bash-script
+if [ $DOWNLOAD ]; then
+
+    # Relevant curl options:
+    # -f  To make HTTP errors turn into a curl error 22, mentioning the HTTP error number
+    # -s  Silent mode
+    # -S  Show errors when in silent mode
+
+    # Any errors of md5sum go temporarily to &3 in order to not overwrite errors from curl
+    # All errors are collected in ERRORS
+    exec 3>&1  # Set up extra file descriptor
+    ERRORS=$( { curl -D ${LOCALPATH}/${LOCALNAME}.headers -f -s -S --header "\"TE: trailers\"" "$YOUSEE_URL_TO_FILE" | tee "${LOCALPATH}/${LOCALNAME}" | md5sum 2>&3 1>"${LOCALPATH}/${LOCALNAME}.md5"; } 2>&1 3>&1 )
+    exec 3>&-  # Release the extra file descriptor
+
+    # The above was inspired by http://stackoverflow.com/questions/962255/how-to-store-standard-error-in-a-variable-in-a-bash-script
+
+fi
 
 
 if [ -z "$ERRORS" ]; then
-	# No errors, so content was downloadable
-	FILESIZE=$(stat -c%s "${LOCALPATH}/${LOCALNAME}")
-	echo '{'
-	echo '   "downloaded":'
-	echo '   {'
-	echo "      \"localFileUrl\" : \"file://${LOCALPATH}/${LOCALNAME}\","
-	echo "      \"checksum\" : \"`cat ${LOCALPATH}/${LOCALNAME}.md5 | sed 's| .*||'`\","
-	echo "      \"fileSize\" : $FILESIZE"
-	echo '   }'
-	echo '}'
-	rm "${LOCALPATH}/${LOCALNAME}.md5" >/dev/null 2>/dev/null
-	exit 0
+    if ! isOK; then
+        echo "File was downloaded but checksums do not match. despair" >&2
+        echo "checksum on stream: $OURCHECKSUM" >&2
+        echo "checksum from yousee: $THEIRCHECKSUM" >&2
+        echo "checksum calculated on disk: $LOCALCHECKSUM" >&2
+        exit 1
+    else
+        # No errors, so content was downloadable
+        FILESIZE=$(stat -c%s "${LOCALPATH}/${LOCALNAME}")
+
+        echo '{'
+        echo '   "downloaded":'
+        echo '   {'
+        echo "      \"localFileUrl\" : \"file://${LOCALPATH}/${LOCALNAME}\","
+        echo "      \"checksum\" : $OURCHECKSUM,"
+        echo "      \"fileSize\" : $FILESIZE"
+        echo '   }'
+        echo '}'
+        #rm "${LOCALPATH}/${LOCALNAME}.md5" >/dev/null 2>/dev/null
+        exit 0
+    fi
 fi
 
 # No more use for these
-rm "${LOCALPATH}/${LOCALNAME}" >/dev/null 2>/dev/null
-rm "${LOCALPATH}/${LOCALNAME}.md5" >/dev/null 2>/dev/null
+rm -rf "${LOCALPATH}/${LOCALNAME}" >/dev/null 2>/dev/null
+rm -rf "${LOCALPATH}/${LOCALNAME}.md5" >/dev/null 2>/dev/null
+rm -rf "${LOCALPATH}/${LOCALNAME}.headers" >/dev/null 2>/dev/null
 
 # So... there are errors. Pick out the error code and act on it
 ERROR_CODE_LINE=`echo $ERRORS | grep 'The requested URL returned error' `
--- src/main/script/yousee.cgi
+++ src/main/script/yousee.cgi
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-#MUX=DR1_20111123_212500_20111123_214500.mux
+#MUX=dr1_20111123212500_20111123214500.mux
 MUX=DRtest_20111123_212500_20111123_214500.mux
+#MUX=test.mux
+SIZE=`ls -l $MUX | cut -d' ' -f5`
+MD5=`md5sum $MUX | cut -d' ' -f1 | base64`
 
 PATH_INFO=`echo "$PATH_INFO" | tr '[:lower:]' '[:upper:]'`
 
 case "$PATH_INFO" in
     /DR*)
 	echo "Content-type: application/octet-stream"
-	echo
+	echo "Transfer-Encoding: chunked"
+	echo "Trailer: Content-MD5"
+	printf '\r\n%x\r\n' $SIZE
 	cat $MUX
+	printf '\r\n%x\r\n' 0
+	echo -n "Content-MD5: $MD5"
+	printf '\r\n'
+
+	printf '\r\n'
 	;;
     /TV2_*)
         echo "Status: 410 Content is not available on any archive server"
